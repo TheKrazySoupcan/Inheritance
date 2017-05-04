c4f65e7f92dc78014caec63040f45b9214f0d667
TheKrazySoupcan <thekrazysoupcan@gmail.com>
2017-05-04 15:37:03 +1000

    Inherit

---
+++ Assets/Script/Enemies/Boomer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class Boomer : Enemy
+{
+    [Header("boomer")]
+    public SphereCollider explosionSphere;
+    public GameObject onSelfDestruct;
+
+    void Update ()
+    {
+        SeekToTarget();
+    }
+
+  void FixedUpdate()
+    {
+
+
+    }
+
+    void SelfDestruct()
+    {
+        PlayExplosion();
+    }
+
+    void PlayExplosion()
+    {
+        GameObject explosion = Instantiate(onSelfDestruct);
+        explosion.transform.position = transform.position;
+    }
+
+    void Explode()
+    {
+        // Detect collision with others
+
+    }
+}
---
+++ Assets/Script/Enemies/Enemy.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class Enemy : MonoBehaviour
+{
+    [Header("Enemy")]
+    public float damage = 50f;
+    public float health = 100f;
+    public float movementSpeed = 20f;
+    public float stoppingDistance = 2f;
+    public Transform target;
+    public NavMeshAgent agent;
+
+
+    void Awake()
+    {
+
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    protected void SeekToTarget ()
+    {
+       if (target != null)
+        {
+            agent.stoppingDistance = stoppingDistance;
+            agent.SetDestination(target.position);
+        }
+    }
+
+    protected bool IsAtTarget ()
+    {
+        return agent.remainingDistance <= agent.stoppingDistance;
+    }
+
+    
+}
